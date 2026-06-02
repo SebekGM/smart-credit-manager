2a6d79685a1305d83bbc4a204ad5897504d1e90b
SebekGM <s.gasior97@gmail.com>
2026-06-02 04:57:23 +0200

    feat: add modal/components

--- src/scripts/modal-overlay.js
+++ src/scripts/modal-overlay.js
@@ -1,21 +1,27 @@
 const modalOverlay = document.getElementById('modal-overlay');
-const creditBtn = document.getElementById('new-credit-btn');
+const creditBtn = document.querySelectorAll('#new-credit-btn');
 const modalCloseBtn = document.getElementById('modal-overlay-close-btn');
 
-creditBtn.addEventListener('click', () => {
-    modalOverlay.classList.add('active');
+// open modal
+creditBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        modalOverlay.classList.add('active');
+    });
 });
 
+// close 'x' btn
 modalCloseBtn.addEventListener('click', () => {
     modalOverlay.classList.remove('active');
 });
 
+// close to click around modal
 modalOverlay.addEventListener('click', (e) => {
     if (e.target === modalOverlay) {
         modalOverlay.classList.remove('active');
     }
 });
 
+// close to click 'Esc' btn on keydown
 document.addEventListener('keydown', (e) => {
     if (e.key === 'Escape' && modalOverlay.classList.contains('active')) {
         modalOverlay.classList.remove('active');
